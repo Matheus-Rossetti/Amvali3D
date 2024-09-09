9268291a9415c860893c86a8cca48c762c5eab66
Matheus-Rossetti <matheuzdreher@gmail.com>
2024-09-09 14:01:53 -0300

    webview funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_3d_controller/flutter_3d_controller.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: WebViewScreen(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Amvali 3D viewer Alpha',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 116, 173, 50)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Amvali 3D viewer Alpha'),
-    );
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+class _WebViewScreenState extends State<WebViewScreen> {
+  late WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+        Uri.parse(r'https://app.speckle.systems/projects/2c12228de1/models/5b2b79388c'),
+      );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Flutter3DController controller = Flutter3DController();
-  String? chosenAnimation;
-  String? chosenTexture;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: const Color.fromARGB(255, 255, 255, 255),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Flutter3DViewer(
-          //If you don't pass progressBarColor the color of defaultLoadingProgressBar will be grey.
-          //You can set your custom color or use [Colors.transparent] for hiding loadingProgressBar.
-            controller: controller,
-            progressBarColor: Colors.cyan,
-            src:
-            //'lib/assets/DamagedHelmet.glb',
-
-            'assets/models/AMV-SH-DE-EDF-0038-PB-R00-3D-Quadra_poliesportiva.glb',
-
-
-          //'https://modelviewer.dev/shared-assets/models/Astronaut.glb', // 3D model from URL
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Amvali3DViewer'),
+      ),
+      body: WebViewWidget(
+        controller: controller,
       ),
     );
-  }
-
-  Future<String?> showPickerDialog(List<String> inputList,
-      [String? chosenItem]) async {
-    return await showModalBottomSheet<String>(
-        context: context,
-        builder: (ctx) {
-          return SizedBox(
-            height: 250,
-            child: ListView.separated(
-              itemCount: inputList.length,
-              padding: const EdgeInsets.only(top: 16),
-              itemBuilder: (ctx, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.pop(context, inputList[index]);
-                  },
-                  child: Container(
-                    height: 50,
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('${index + 1}'),
-                        Text(inputList[index]),
-                        Icon(chosenItem == inputList[index]
-                            ? Icons.check_box
-                            : Icons.check_box_outline_blank)
-                      ],
-                    ),
-                  ),
-                );
-              },
-              separatorBuilder: (ctx, index) {
-                return const Divider(
-                  color: Colors.grey,
-                  thickness: 0.6,
-                  indent: 10,
-                  endIndent: 10,
-                );
-              },
-            ),
-          );
-        });
   }
 }
