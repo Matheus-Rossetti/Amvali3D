58dc1219e5cf6bd6a206b7be6be8f63b7cbc2372
Matheus-Rossetti <matheuzdreher@gmail.com>
2024-10-16 09:16:48 -0300

    Maybe I'll rollback to this

--- lib/login.dart
+++ lib/login.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 
 final TextEditingController emailController = TextEditingController();
 final TextEditingController passwordController = TextEditingController();
--- lib/navbar_screens/projects.dart
+++ lib/navbar_screens/projects.dart
@@ -3,6 +3,11 @@ import 'package:amvali3dviewer/web_view.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
+
+
+int totalCount = 5;
+
+
 class ProjectsScreen extends StatelessWidget {
   const ProjectsScreen({super.key});
 
@@ -10,36 +15,41 @@ class ProjectsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: HexColor('#005190'),
-        body: ListView(
-          children: List.generate(10, (index) {
-            return Center(
-                child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const WebView()));
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        spreadRadius: 0.1,
-                        // Adjusted value for visibility
-                        blurRadius: 5,
-                        offset: Offset(2, 3),
+        body: CustomScrollView(
+          slivers: [
+            ListView(
+              children: List.generate(totalCount, (index) {
+                return Center(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => const WebView()));
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 230,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            spreadRadius: 0.1,
+                            // Adjusted value for visibility
+                            blurRadius: 5,
+                            offset: Offset(2, 3),
+                          ),
+                        ],
                       ),
-                    ],
+                      child: Image.network("http://20.201.114.134/preview/32b1a0c3bd/commits/446108e3a7"),
+                    ),
                   ),
-                ),
-              ),
-            ));
-          }),
+                ));
+              }),
+            ),
+          ],
         ),
         bottomNavigationBar: const NavBar());
   }
